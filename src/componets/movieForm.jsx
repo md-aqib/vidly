d1015751ff704fe47ea2e6ab267d2dc73202527a
aquibmax <mdaquib327@gmail.com>
2020-07-21 15:35:28 +0530

    movieForm issue fixed

--- src/componets/movieForm.jsx
+++ src/componets/movieForm.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { getMovie, saveMovie } from "../services/movieService";
 import { getGenres } from "../services/genreService";
-import { getMovies, saveMovie } from "./../services/movieService";
+
 class MovieForm extends Form {
   state = {
-    data: { title: "", genreId: "", numberInStock: "", dailyRentalRate: "" },
+    data: {
+      title: "",
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: "",
+    },
     genres: [],
     errors: {},
   };
+
   schema = {
     _id: Joi.string(),
     title: Joi.string().required().label("Title"),
@@ -17,33 +24,38 @@ class MovieForm extends Form {
       .required()
       .min(0)
       .max(100)
-      .label("Number In Stock"),
+      .label("Number in Stock"),
     dailyRentalRate: Joi.number()
       .required()
       .min(0)
       .max(10)
       .label("Daily Rental Rate"),
   };
+
   async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
   }
+
   async populateMovie() {
     try {
       const movieId = this.props.match.params.id;
       if (movieId === "new") return;
-      const { data: movie } = await getMovies(movieId);
+
+      const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         this.props.history.replace("/not-found");
     }
   }
+
   async componentDidMount() {
     await this.populateGenres();
     await this.populateMovie();
   }
-  mapToViewModel = (movie) => {
+
+  mapToViewModel(movie) {
     return {
       _id: movie._id,
       title: movie.title,
@@ -51,11 +63,14 @@ class MovieForm extends Form {
       numberInStock: movie.numberInStock,
       dailyRentalRate: movie.dailyRentalRate,
     };
-  };
+  }
+
   doSubmit = async () => {
     await saveMovie(this.state.data);
+
     this.props.history.push("/movies");
   };
+
   render() {
     return (
       <div>
@@ -63,7 +78,7 @@ class MovieForm extends Form {
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("title", "Title")}
           {this.renderSelect("genreId", "Genre", this.state.genres)}
-          {this.renderInput("numberInStock", "Number In Stock", "number")}
+          {this.renderInput("numberInStock", "Number in Stock", "number")}
           {this.renderInput("dailyRentalRate", "Rate")}
           {this.renderButton("Save")}
         </form>
